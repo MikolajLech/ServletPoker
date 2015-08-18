84299f9edc82855cdda83ada840b1699dfeb0493
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-08-18 10:39:56 +0200

    Initial commit

---
+++ ServletPoker/src/milech/Hello.java
@@ -0,0 +1,35 @@
+package milech;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public Hello() {}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("user");
+		out.print("Hello " + name);
+		out.close();
+	}
+
+}
